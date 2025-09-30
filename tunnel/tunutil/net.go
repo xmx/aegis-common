4504465389522869bdf1cae4d2aa1aa14f88aecc
wzy <hnwzy@live.com>
2025-09-30 15:09:17 +0800

    update

--- tunnel/tunutil/net.go
+++ tunnel/tunutil/net.go
@@ -11,10 +11,54 @@ type Dialer interface {
 	DialContext(ctx context.Context, network, address string) (net.Conn, error)
 }
 
-type Server interface {
-	Serve(net.Listener) error
+type MatchedDialer interface {
+	Dialer
+	DialMatched(network, host, port string) bool
+}
+
+func DefaultDialer() Dialer {
+	return &defaultDialer{}
+}
+
+type defaultDialer struct {
+	d net.Dialer
+}
+
+func (d defaultDialer) DialContext(ctx context.Context, network, address string) (net.Conn, error) {
+	return d.d.DialContext(ctx, network, address)
 }
 
 type Handler interface {
 	Handle(tundial.Muxer) error
+}
+
+func NewMatchDialer(fallback Dialer, matches ...MatchedDialer) Dialer {
+	if fallback == nil {
+		fallback = DefaultDialer()
+	}
+
+	return &matchedDialer{
+		matches:  matches,
+		fallback: fallback,
+	}
+}
+
+type matchedDialer struct {
+	matches  []MatchedDialer
+	fallback Dialer
+}
+
+func (md *matchedDialer) DialContext(ctx context.Context, network, address string) (net.Conn, error) {
+	host, port, err := net.SplitHostPort(address)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, m := range md.matches {
+		if m.DialMatched(network, host, port) {
+			return m.DialContext(ctx, network, address)
+		}
+	}
+
+	return md.fallback.DialContext(ctx, network, address)
 }
