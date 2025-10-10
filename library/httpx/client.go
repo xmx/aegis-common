f1b5defa45ee8c94b0d2c5acbb0567192aaf9764
wzy <hnwzy@live.com>
2025-10-10 14:08:48 +0800

    http client

--- library/httpx/client.go
+++ library/httpx/client.go
@@ -3,52 +3,60 @@ package httpx
 import (
 	"bytes"
 	"context"
-	"encoding/json"
+	"encoding/json/v2"
 	"io"
 	"net/http"
 	"net/url"
 	"strings"
 )
 
+func NewClient(c *http.Client) Client {
+	return Client{cli: c}
+}
+
 // Client http 客户端。
-//
-// usage:
-//
-//	cli := &httpx.Client{Client: http.DefaultClient}
 type Client struct {
-	Client *http.Client
+	cli *http.Client
 }
 
-// JSON GET request and response JSON
-func (c Client) JSON(ctx context.Context, rawURL string, header http.Header, result any) error {
-	resp, err := c.sendJSON(ctx, http.MethodGet, rawURL, header, nil)
+// GetJSON GET request and response JSON
+func (c Client) GetJSON(ctx context.Context, rawURL string, header http.Header, result any) error {
+	resp, err := c.sendJSON(ctx, http.MethodGet, rawURL, header, nil, true)
 	if err != nil {
 		return err
 	}
-	err = c.unmarshalJSON(resp.Body, result)
 
-	return err
+	return c.unmarshalJSON(resp.Body, result)
 }
 
-// PostJSON POST JSON request and response JSON
-func (c Client) PostJSON(ctx context.Context, rawURL string, header http.Header, body, result any) error {
-	resp, err := c.sendJSON(ctx, http.MethodPost, rawURL, header, body)
+func (c Client) SendJSON(ctx context.Context, method, rawURL string, header http.Header, body, result any) error {
+	if method == "" {
+		method = http.MethodPost
+	}
+
+	resp, err := c.sendJSON(ctx, method, rawURL, header, body, false)
 	if err != nil {
 		return err
 	}
-	err = c.unmarshalJSON(resp.Body, result)
 
-	return err
+	return c.unmarshalJSON(resp.Body, result)
 }
 
-func (c Client) PostForm(ctx context.Context, rawURL string, header http.Header, body url.Values, result any) error {
+func (c Client) SendURLEncoded(ctx context.Context, method, rawURL string, header http.Header, body url.Values, result any) error {
+	if method == "" {
+		method = http.MethodPost
+	}
 	if header == nil {
 		header = make(http.Header, 4)
 	}
 	header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-	encode := body.Encode()
-	req, err := c.newRequest(ctx, http.MethodPost, rawURL, header, strings.NewReader(encode))
+	var r io.Reader
+	if len(body) != 0 {
+		raw := body.Encode()
+		r = strings.NewReader(raw)
+	}
+	req, err := c.newRequest(ctx, method, rawURL, header, r)
 	if err != nil {
 		return err
 	}
@@ -58,23 +66,21 @@ func (c Client) PostForm(ctx context.Context, rawURL string, header http.Header,
 		return err
 	}
 
-	err = c.unmarshalJSON(resp.Body, result)
-
-	return err
+	return c.unmarshalJSON(resp.Body, result)
 }
 
 func (c Client) Do(req *http.Request) (*http.Response, error) {
 	return c.send(req)
 }
 
-func (c Client) sendJSON(ctx context.Context, method, rawURL string, header http.Header, body any) (*http.Response, error) {
+func (c Client) sendJSON(ctx context.Context, method, rawURL string, header http.Header, body any, nobody bool) (*http.Response, error) {
 	if header == nil {
 		header = make(http.Header, 4)
 	}
 	header.Set("Accept", "application/json")
 
 	var r io.Reader
-	if method != http.MethodGet && method != http.MethodHead {
+	if !nobody {
 		rd, err := c.marshalJSON(body)
 		if err != nil {
 			return nil, err
@@ -112,12 +118,15 @@ func (c Client) send(req *http.Request) (*http.Response, error) {
 	if host := h.Get("Host"); host != "" {
 		req.Host = host
 	}
+	if h.Get("Accept") == "" {
+		h.Set("Accept", "*/*")
+	}
 	if h.Get("User-Agent") == "" {
-		chrome127 := "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
-		h.Set("User-Agent", chrome127)
+		chrome141 := "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"
+		h.Set("User-Agent", chrome141)
 	}
 	if h.Get("Accept-Language") == "" {
-		h.Set("Accept-Language", "zh-CN,zh;q=0.9")
+		h.Set("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6")
 	}
 
 	resp, err := c.getClient().Do(req)
@@ -146,23 +155,30 @@ func (c Client) send(req *http.Request) (*http.Response, error) {
 
 func (c Client) marshalJSON(v any) (io.Reader, error) {
 	buf := new(bytes.Buffer)
-	err := json.NewEncoder(buf).Encode(v)
-	return buf, err
+	if err := json.MarshalWrite(buf, v); err != nil {
+		return nil, err
+	}
+
+	return buf, nil
 }
 
 func (c Client) getClient() *http.Client {
-	if cli := c.Client; cli != nil {
+	if cli := c.cli; cli != nil {
 		return cli
 	}
 	return http.DefaultClient
 }
 
-func (c Client) unmarshalJSON(rc io.ReadCloser, v any) error {
+func (c Client) unmarshalJSON(rc io.ReadCloser, result any) error {
 	//goland:noinspection GoUnhandledErrorResult
 	defer rc.Close()
-	if v == nil || rc == http.NoBody {
+	if rc == http.NoBody {
 		return nil
 	}
+	if result == nil {
+		_, err := io.Copy(io.Discard, rc)
+		return err
+	}
 
-	return json.NewDecoder(rc).Decode(v)
+	return json.UnmarshalRead(rc, result)
 }
