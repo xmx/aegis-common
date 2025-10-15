290ce41ade5c650459b45ecf37c80ab3f3d38ad6
wzy <hnwzy@live.com>
2025-10-15 14:27:53 +0800

    websocket io copy

--- library/wsocket/upgrade.go
+++ library/wsocket/upgrade.go
@@ -1,16 +1,26 @@
 package wsocket
 
 import (
+	"context"
+	"net"
 	"net/http"
 	"time"
 
 	"github.com/gorilla/websocket"
 )
 
-func NewUpgrade() *websocket.Upgrader {
+func DefaultUpgrader() *websocket.Upgrader {
 	return &websocket.Upgrader{
 		HandshakeTimeout:  10 * time.Second,
 		CheckOrigin:       func(*http.Request) bool { return true },
+		EnableCompression: true,
+	}
+}
+
+func NewDialer(dc func(ctx context.Context, network, addr string) (net.Conn, error)) *websocket.Dialer {
+	return &websocket.Dialer{
+		NetDialContext:    dc,
+		HandshakeTimeout:  10 * time.Second,
 		EnableCompression: true,
 	}
 }
