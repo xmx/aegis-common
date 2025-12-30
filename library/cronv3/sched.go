08462fa36d206e0fcb04de4f0826eaa81def59e0
WangZY <you@example.com>
2025-12-30 17:32:52 +0800

    refactor(cronv3): 删除 NewInterval 定期执行支持并简化 TaskInfo 结构体
    - 移除了 NewInterval 函数及相关 intervalTimes 结构，实现简化
    - 删去 TaskInfo 结构体中所有 json 标签，调整字段定义
    - 精简代码，去除多余注释和配置，提高维护性
    - 保持原有功能同时优化代码风格和结构

--- library/cronv3/sched.go
+++ library/cronv3/sched.go
@@ -40,25 +40,3 @@ func (st *specificTimes) Next(now time.Time) time.Time {
 
 	return time.Time{}
 }
-
-// NewInterval 定期执行。
-// 不同于 cron 表达式的是：cron/v3 时间粒度（秒级/分钟级）是可配置的，
-// 不同的级别对应的 cron 表达式位数不一样。
-// 此方式可以自适应不同级别的 cron/v3，为了两者兼容，所以限制最小间隔为一分钟。
-func NewInterval(du time.Duration) cron.Schedule {
-	//if du < time.Minute {
-	//	du = time.Minute
-	//}
-
-	return &intervalTimes{
-		du: du,
-	}
-}
-
-type intervalTimes struct {
-	du time.Duration
-}
-
-func (it *intervalTimes) Next(now time.Time) time.Time {
-	return now.Add(it.du)
-}
